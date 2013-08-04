f2fa8e1d54468b61d2213a18163caf259b6c3c07
Jeff Cosgriff <jeff.cosgriff@me.com>
2013-08-04 14:36:53 -0700

    adding the precompile flag for production

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe "Static Pages" do
   describe "Home Page" do
-    before { visit root_path}
+    before { visit root_path }
     it "should have the content 'Sample App'" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
 	     page.should have_content('Sample App')
